3651ffa026a1c15156d7d69255aa623e1a75ac09
Saifur <ranacse@gmail.com>
2019-12-09 11:40:29 +0600

    add get URL..2nd commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private http:HttpClient) { }
+
+  employees: any;
+  products : any;
+  ngOnInit(): void {
+
+    
+    var sdf= this.asd();
+    var asd= this.products;
+    
+    
+  }
+  
+  
   title = 'dockerAngu';
+  asd(){
+    var fetchData = 
+      this.http.get("https://api.github.com/users/mralexgray/repos").subscribe(
+        (res: Response) => {
+          this.products = res;
+          debugger
+          
+        }
+      )
+    
+    console.log("Hello" + this.products + " no asd");
+
+  }
+  
+  
+  
+
 }
